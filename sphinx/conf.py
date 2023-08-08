42bc0ca55504b8a4e82acdbaaf8b61d3acaf80a4
kathryn-garside <Kathryn.garside@newcastle.ac.uk>
2023-08-08 13:41:01 +0100

    Update conf.py

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -1,3 +1,13 @@
-extensions = ['sphinx_rtd_theme','sphinx.ext.autosummary']
+extensions = ['sphinx_rtd_theme','sphinx.ext.autosummary',"sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+
+templates_path = ["_templates"]
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
+
+html_static_path = ["_static"]
+
+html_css_files = [
+    "css/custom.css",
+]
